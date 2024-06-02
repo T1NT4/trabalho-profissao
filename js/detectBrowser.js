0da12881d6bb139d9e03b45ef8701c8ebc02395b
T1NT4 <thiagogabriel.osil@gmail.com>
2024-06-01 21:07:41 -0300

    iphone added to the mirrored emoji condition

--- js/detectBrowser.js
+++ js/detectBrowser.js
@@ -2,3 +2,6 @@ var user_agent = navigator.userAgent
 if(user_agent.toLowerCase().includes("android")){
     document.getElementsByClassName("logo-emoji")[0].classList.add("android")
 }
+if(user_agent.toLowerCase().includes("iphone")){
+    document.getElementsByClassName("logo-emoji")[0].classList.add("iphone")
+}
