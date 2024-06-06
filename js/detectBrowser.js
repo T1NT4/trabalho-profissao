37ddb5f5525b0c0ab141cf65ec3dc959855656f7
T1NT4 <thiagogabriel.osil@gmail.com>
2024-06-05 22:11:38 -0300

    the header now changes if you scroll

--- js/detectBrowser.js
+++ js/detectBrowser.js
@@ -5,3 +5,17 @@ if(user_agent.toLowerCase().includes("android")){
 if(user_agent.toLowerCase().includes("iphone")){
     document.getElementsByClassName("logo-emoji")[0].classList.add("iphone")
 }
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementsByTagName("header")[0];
+
+var sticky = 100;
+
+function myFunction() {
+  if (window.scrollY > sticky) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+}
